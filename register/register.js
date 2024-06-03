264c03541f377b7ec69fd7de7d0402fa95cccf27
Jethro Tuason <151513908+Jethrotuason@users.noreply.github.com>
2024-06-03 22:47:29 +0800

    final register

---
+++ register/register.js
@@ -0,0 +1,12 @@
+function matchPassword() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm_password").value;
+    if (password != confirmPassword) {
+        alert("Passwords do not match.");
+        return false;
+    }
+    return true;
+}
+document.getElementById("cancelBTN").addEventListener("click", function() {
+    history.back();
+});
